c57d6e2a291a107ad6988f6bc829f5bb13799592
MathisCABOSTE <mathis.caboste@epita.fr>
2023-01-09 18:59:13 +0100

    Messages de lancement du serveur

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -8,10 +8,15 @@ public class Launcher {
     public static void main(String[] args) throws java.io.IOException {
         try {
             var server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
+            System.out.println("Creating context '/ping' ...");
             server.createContext("/ping", new HttpRequestHandler());
+            System.out.println("Creating context '/api/game/start' ...");
             server.createContext("/api/game/start", new ApiGameStart());
+            System.out.println("Setting executor ...");
             server.setExecutor(null);
+            System.out.println("Starting server ...");
             server.start();
+            System.out.println("Server started.");
         } catch (IOException e) {
             e.printStackTrace();
         }
