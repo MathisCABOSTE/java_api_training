4fe3654f99d31f5e567e87518d57820a8d974587
MathisCABOSTE <mathis.caboste@epita.fr>
2023-01-10 18:22:31 +0100

    First API Setup

--- src/main/java/fr/lernejo/navy_battle/ApiGameStart.java
+++ src/main/java/fr/lernejo/navy_battle/ApiGameStart.java
@@ -1,15 +1,35 @@
 package fr.lernejo.navy_battle;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class ApiGameStart implements HttpHandler {
+    int port;
+    String id;
+    public void init(int port, String id){
+        this.port = port;
+        this.id = id;
+    }
     public void handle(HttpExchange exchange) throws IOException {
-        String body = exchange.getRequestBody().toString();
+        String body = "{ \"id\":\""+ this.id + "\",\"url\":\"http://localhost:"+ Integer.toString(this.port) +"\",\"message\": \"Get ready for the battle !\"}";
         try (OutputStream os = exchange.getResponseBody()) {
-            os.write(body.getBytes());
-            os.close();
+            if (exchange.getRequestMethod().equals("POST")){
+                String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+                System.out.println(requestBody);
+                if (requestBody.contains("id") && requestBody.contains("url") && requestBody.contains("message")){
+                    exchange.sendResponseHeaders(200, body.length());
+                    os.write(body.getBytes());
+                }
+
+            } else {
+                body = "404 (Not Found)";
+                exchange.sendResponseHeaders(404,body.length());
+                os.write(body.getBytes());
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
             throw e;
--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -3,20 +3,42 @@ package fr.lernejo.navy_battle;
 import java.io.*;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.UUID;
 
 public class Launcher {
-    public static void main(String[] args) throws java.io.IOException {
+    public static void main(String[] args) throws java.io.IOException, java.lang.InterruptedException {
         try {
-            var server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
+            int port = Integer.parseInt(args[0]);
+            String id = UUID.randomUUID().toString();
+            var server = HttpServer.create(new InetSocketAddress(port), 0);
             System.out.println("Creating context '/ping' ...");
-            server.createContext("/ping", new HttpRequestHandler());
+            server.createContext("/ping", new Ping());
             System.out.println("Creating context '/api/game/start' ...");
-            server.createContext("/api/game/start", new ApiGameStart());
+            ApiGameStart apiGameStart = new ApiGameStart();
+            apiGameStart.init(port, id);
+            server.createContext("/api/game/start", apiGameStart);
             System.out.println("Setting executor ...");
             server.setExecutor(null);
             System.out.println("Starting server ...");
             server.start();
             System.out.println("Server started.");
+            if (args.length > 1) {
+                String target = args[1];
+                HttpClient httpClient = HttpClient.newHttpClient();
+                String gameStartJson = "{ \"id\":\""+ id + "\",\"url\":\"http://localhost:"+ Integer.toString(port) +"\",\"message\": \"I'm dumb. But let's play anyway :D\"}";
+                HttpRequest postRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(target + "/api/game/start"))
+                    .setHeader("Accept", "application/json")
+                    .setHeader("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(gameStartJson))
+                    .build();
+                var response = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+                System.out.println(response.body());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
