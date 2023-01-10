6ea15c4b2f6255b83302484c8e55d0ff2fb59e30
MathisCABOSTE <mathis.caboste@epita.fr>
2023-01-10 17:22:43 +0100

    HttpRequestHandler.java changed to Ping.java for better lisibility

---
+++ src/main/java/fr/lernejo/navy_battle/Ping.java
@@ -0,0 +1,18 @@
+package fr.lernejo.navy_battle;
+
+import java.io.*;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class Ping implements HttpHandler {
+    public void handle(HttpExchange exchange) throws IOException {
+        String body = "Hello";
+        exchange.sendResponseHeaders(200, body.length());
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(body.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
