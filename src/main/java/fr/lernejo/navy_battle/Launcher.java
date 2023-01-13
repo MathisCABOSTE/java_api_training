6329d859a26890a35c1bb562d023f9c06af7d732
MathisCABOSTE <mathis.caboste@epita.fr>
2023-01-13 14:01:27 +0100

    main function was simplified : creation of the createContexts and sendGameStartjson functions

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -10,34 +10,39 @@ import java.net.http.HttpResponse;
 import java.util.UUID;
 
 public class Launcher {
-    public static void main(String[] args) throws java.io.IOException, java.lang.InterruptedException {
+    public static void createContexts(int port, String id) throws  IOException{
+        var server = HttpServer.create(new InetSocketAddress(port), 0);
+        System.out.println("Creating context '/ping' ...");
+        server.createContext("/ping", new Ping());
+        System.out.println("Creating context '/api/game/start' ...");
+        ApiGameStart apiGameStart = new ApiGameStart();
+        apiGameStart.init(port, id);
+        server.createContext("/api/game/start", apiGameStart);
+        System.out.println("Setting executor ...");
+        server.setExecutor(null);
+        System.out.println("Starting server ...");
+        server.start();
+        System.out.println("Server started.");
+    }
+    public static HttpResponse<String> sendGameStartJson(HttpClient httpClient, int port, String id, String target) throws InterruptedException, IOException{
+        String gameStartJson = "{ \"id\":\""+ id + "\",\"url\":\"http://localhost:"+ Integer.toString(port) +"\",\"message\": \"I'm dumb. But let's play anyway :D\"}";
+        HttpRequest postRequest = HttpRequest.newBuilder()
+            .uri(URI.create(target + "/api/game/start"))
+            .setHeader("Accept", "application/json")
+            .setHeader("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(gameStartJson))
+            .build();
+        return httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+    }
+    public static void main(String[] args) throws InterruptedException{
         try {
             int port = Integer.parseInt(args[0]);
             String id = UUID.randomUUID().toString();
-            var server = HttpServer.create(new InetSocketAddress(port), 0);
-            System.out.println("Creating context '/ping' ...");
-            server.createContext("/ping", new Ping());
-            System.out.println("Creating context '/api/game/start' ...");
-            ApiGameStart apiGameStart = new ApiGameStart();
-            apiGameStart.init(port, id);
-            server.createContext("/api/game/start", apiGameStart);
-            System.out.println("Setting executor ...");
-            server.setExecutor(null);
-            System.out.println("Starting server ...");
-            server.start();
-            System.out.println("Server started.");
+            createContexts(port, id);
             if (args.length > 1) {
                 String target = args[1];
                 HttpClient httpClient = HttpClient.newHttpClient();
-                String gameStartJson = "{ \"id\":\""+ id + "\",\"url\":\"http://localhost:"+ Integer.toString(port) +"\",\"message\": \"I'm dumb. But let's play anyway :D\"}";
-                HttpRequest postRequest = HttpRequest.newBuilder()
-                    .uri(URI.create(target + "/api/game/start"))
-                    .setHeader("Accept", "application/json")
-                    .setHeader("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(gameStartJson))
-                    .build();
-                var response = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
-                System.out.println(response.body());
+                var response = sendGameStartJson(httpClient,port,id,target);
             }
         } catch (IOException e) {
             e.printStackTrace();
